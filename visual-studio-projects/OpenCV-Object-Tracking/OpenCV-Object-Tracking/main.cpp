eedaf78b4d4a04e256e846f08b95cb687657a5f2
Dillon Fisher <thirty9th@gmail.com>
2014-04-26 12:27:03 -0500

    Updated to homography-based tracking
    * Made tracking work with drawing homography points
    * Bounding box still jumps around a lot; need to fine tune

--- visual-studio-projects/OpenCV-Object-Tracking/OpenCV-Object-Tracking/main.cpp
+++ visual-studio-projects/OpenCV-Object-Tracking/OpenCV-Object-Tracking/main.cpp
@@ -11,11 +11,14 @@
 /*
  * 	Includes
  */
-#include <opencv2/opencv.hpp>
-#include <opencv2/core.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/objdetect.hpp>
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/nonfree/nonfree.hpp"
+#include "opencv2/flann/flann.hpp"
 #include <iostream>
 #include <fstream>
 
@@ -28,12 +31,13 @@ using namespace cv;
  * 	Globals
  */
 // Filenames
-const std::string imageDir = "images/";
-const std::string videoDir = "videos/";
-const std::string sourceVideo = "source.mp4";
+const std::string imageDir = "images/";         // Directory for images
+const std::string videoDir = "videos/";         // Directory for videos
+const std::string sourceVideo = "source2.mp4";   // Source video
+const std::string objectProfile = "pen.png";    // Object to be detected within video
 
 // Variables
-int trackHueMin, trackHueMax;   // Threshhold min and max
+const int minimumHessian = 400;                 // Parameter for feature detector
 
 /*
  * 	Defines
@@ -49,12 +53,10 @@ int trackHueMin, trackHueMax;   // Threshhold min and max
 void printHeader();
 bool loadImage(Mat&, std::string);
 void displayImage(Mat&, std::string, bool);
-void displayVideo(VideoCapture&, std::string);
-void threshholdImage(Mat&, Mat&);
-int displayThreshholdedFrames(VideoCapture&, const std::string&);
-void showThreshholdTrackbars(const std::string);
+void displayVideo(VideoCapture&, const std::string);
+int detectObjectInVideo(VideoCapture&, Mat*, const std::string);
 
-int main(int argc, char* const argv[])
+int main(int argc, const char* argv[])
 {
 
 	// Show greeting header
@@ -63,29 +65,39 @@ int main(int argc, char* const argv[])
 	// Load images into buffer from video source
 	// Start capture using a source video location
 	std::string filename = videoDir + sourceVideo;
-	VideoCapture capture("source.mp4");
+	VideoCapture capture(filename);
 
 	// Check that it opened properly
 	if (!capture.isOpened())
 	{
         // Exit with error
-		std::cout << "$ Unable to open stream from <" << filename << ">." << std::endl;
+		std::cout << "$ ERROR: Unable to open stream from <" << filename << ">." << std::endl;
 		std::cin.get();
         return EXIT_ERROR;
     }
+    else
+    {
+        // Provide feedback to let the user know loading was successful
+        std::cout << "$ Video source opened from <" << filename << ">." << std::endl;
+    }
 
-	// Loop through the video frames, threshhold them and display them
-    const std::string threshholdedFramesWindow = "Threshholded-Frames";
-    const std::string trackbarsWindow = "Threshhold-Adjustments";
-	namedWindow(threshholdedFramesWindow, 1);
-    namedWindow(trackbarsWindow, 1);
-    showThreshholdTrackbars(trackbarsWindow);
-	int totalFrames = displayThreshholdedFrames(capture, "Threshholded-Frames");
-    std::cout << "$ Total frames in video source: " << totalFrames << std::endl;
+    // Load object profile that will be detected
+    Mat objectToDetect;
+    if(!loadImage(objectToDetect, objectProfile)) 
+    {
+        std::cin.get();
+        return EXIT_ERROR;
+    }
+
+    // Perform 2D homography on the video frames and draw bounding boxes on predictions
+    // of where the object is
+    const std::string detectionWindow = "Homography-Results";
+    namedWindow(detectionWindow, 1);
+    detectObjectInVideo(capture, &objectToDetect, detectionWindow);
 
 	// Exit successfully
 	std::cout << "$ Program terminated successfully." << std::endl;
-    std::cin.get();
+    std::cin.get();                                                 // Wait for keypress
 	return EXIT_SUCCESS;
 
 }
@@ -118,7 +130,7 @@ bool loadImage(Mat &input, std::string filename)
 	}
 
 	// Load desired image
-	input = imread(imageDir + filename, 1);
+	input = imread(imageDir + filename, 0); // Grayscale
 
 	// Check that it loaded correctly
 	if (!input.data)
@@ -158,93 +170,132 @@ void displayImage(Mat& input, std::string windowName, bool waitForKey)
  * 	press a key before closing it
  *
  */
-void displayVideo(VideoCapture& capture, std::string windowName)
+void displayVideo(VideoCapture& capture, const std::string windowName)
 {
 
 	// Loop through video frames
-	Mat frame;
+	Mat frame, frameGray;
 	int count = 0;
 	while(1)
 	{
-		capture >> frame;			// Get current frame image
-		if (count == 0)
-		{
-			Mat* output = new Mat(frame.size(), CV_8U);
-			threshholdImage(frame, *output);
-			displayImage(*output, "testTreshhold", true);
-		}
-		if (frame.empty()) break;	// End of video
-		imshow(windowName, frame);	// Display frame in window
-		waitKey(20);
+		capture >> frame;			                // Get current frame image
+		if (frame.empty()) break;	                // Check for end of video
+        cvtColor(frame, frameGray, COLOR_BGR2GRAY);
+		imshow(windowName, frameGray);	            // Display frame in window
+		waitKey(20);                                // 20ms delay between frame displays
 		count++;
 	}
-	waitKey(0);					// Press key to close window
+	waitKey(0);					                    // Press key to close window
 
 }
 
 /*
  *
- * 	threshholdImage - returns a binary threshholded version of the input image
+ * 	detectObject - uses 2D homography to find a given object within a video; draws a bounding box around
+ *  the object and displays the frames of the video to the screen; returns number of frames in the video
  *
  */
-void threshholdImage(Mat& input, Mat& output)
+int detectObjectInVideo(VideoCapture& capture, Mat* objectToDetect, const std::string windowName)
 {
 
-	// Perform the threshholding operation by checking if each pixel is in the proper
-	// range of values
-    Scalar minimum = (trackHueMin, 0, 0);    // Grab trackbar values
-    Scalar maximum = (trackHueMax, 255, 255);
-	inRange(input, minimum, maximum, output);
+    
+    // Loop variables
+    Mat frame;
+    int count = 0;
 
-}
+    // Feature detection variables
+    Mat frameGray, objectDescriptors, frameDescriptors, combinedMatches, homography;
+    SurfFeatureDetector detector(minimumHessian);
+    std::vector<KeyPoint> objectKeypoints, frameKeypoints;
+    SurfDescriptorExtractor extractor;
+    FlannBasedMatcher matcher;
+    std::vector<DMatch> matches, keptMatches;
+    double maxDistance = 0.0;
+    double minDistance = 100.0;
+    double thisDistance;
+    std::vector<Point2f> objectPoints, framePoints;
+    std::vector<Point2f> objectCorners(4);
+    std::vector<Point2f> frameCorners(4);
 
-/*
- *
- * 	displayThreshholdedFrames - uses a window to display, frame-by-frame, the contents of a
- *  video given a certain threshhold
- *
- */
-int displayThreshholdedFrames(VideoCapture& capture, const std::string& windowName)
-{
-	
-    // Grab each frame in the video, threshhold it and display it
-    Mat frame, threshholdedFrame, frameHSV;
-	int frameCount = 0;
-	while(1)
-	{
-        // Get current frame image
-		capture >> frame;
+    // Pre-compute object keypoints and descriptors
+    detector.detect(*objectToDetect, objectKeypoints);
+    extractor.compute(*objectToDetect, objectKeypoints, objectDescriptors);
 
-        // Check for end conditions
-		if (frame.empty()) return frameCount;	// End of video
-        else
+    // For each video frame, convert it to grayscale and process it
+    while (1)
+    {
+        capture >> frame;			                // Get current frame image
+		if (frame.empty()) break;	                // Check for end of video
+        cvtColor(frame, frameGray, COLOR_BGR2GRAY); // Convert frame to grayscale
+
+        // Cleanup
+        objectPoints.clear();
+        framePoints.clear();
+        matches.clear();
+        keptMatches.clear();
+
+        // Detect keypoints and calculate feature vectors of video frame
+        detector.detect(frameGray, frameKeypoints);
+        extractor.compute(frameGray, frameKeypoints, frameDescriptors);
+
+        // FLANN matching between descriptors
+        matcher.match(objectDescriptors, frameDescriptors, matches);
+
+        // Calculate minimum and maximum distance between keypoints
+        thisDistance = 0.0;
+        for (int i = 0; i < objectDescriptors.rows; i++)
         {
-            // First convert frame to HSV color space for better threshhold results
-            frame.convertTo(frameHSV, COLOR_RGB2HSV);
-
-            // Apply threshholding routine
-		    threshholdImage(frameHSV, threshholdedFrame);
-		    displayImage(threshholdedFrame, windowName, false);
-		    waitKey(20);            // Wait 20ms to display next frame
-		    frameCount++;           // Update frame counter
+            // Update min and max distances as needed
+            thisDistance = matches[i].distance;
+            if (thisDistance < minDistance) minDistance = thisDistance;
+            if (thisDistance > maxDistance) maxDistance = thisDistance;
         }
+
+        // Keep only descriptor matches that are within 3 times the minimum distance
+        for (int i = 0; i < objectDescriptors.rows; i++)
+        {
+            if (matches[i].distance < 1.1 * max(objectToDetect->rows, objectToDetect->cols)) keptMatches.push_back(matches[i]);
+        }
+
+        // Draw the remaining matches
+        drawMatches(*objectToDetect, objectKeypoints, frameGray, frameKeypoints, keptMatches, combinedMatches, Scalar::all(-1), Scalar::all(-1), std::vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+
+        // Localize the object
+        for (int i = 0; i < keptMatches.size(); i++)
+        {
+            objectPoints.push_back(objectKeypoints[keptMatches[i].queryIdx].pt);
+            framePoints.push_back(frameKeypoints[keptMatches[i].trainIdx].pt);
+        }
+
+        // Find homography
+        homography.release();
+        homography = findHomography(Mat(objectPoints), Mat(framePoints), RANSAC, 0.1);
+        if (homography.empty()) continue;
+
+        // Corners of source image for object profile
+        objectCorners[0] = Point2f(0, 0);
+        objectCorners[1] = Point2f(objectToDetect->cols, 0);
+        objectCorners[2] = Point2f(objectToDetect->cols, objectToDetect->rows);
+        objectCorners[3] = Point2f(0, objectToDetect->rows);
+
+        // Perspective drawing
+        perspectiveTransform(objectCorners, frameCorners, homography);
+
+        // Draw lines between corners 1 -> 2, 2 -> 3... etc
+        line(combinedMatches, frameCorners[0] + Point2f(objectToDetect->cols, 0), frameCorners[1] + Point2f(objectToDetect->cols, 0), Scalar(0, 255, 0), 4);
+        line(combinedMatches, frameCorners[1] + Point2f(objectToDetect->cols, 0), frameCorners[2] + Point2f(objectToDetect->cols, 0), Scalar(0, 255, 0), 4);
+        line(combinedMatches, frameCorners[2] + Point2f(objectToDetect->cols, 0), frameCorners[3] + Point2f(objectToDetect->cols, 0), Scalar(0, 255, 0), 4);
+        line(combinedMatches, frameCorners[3] + Point2f(objectToDetect->cols, 0), frameCorners[0] + Point2f(objectToDetect->cols, 0), Scalar(0, 255, 0), 4);
+
+        // Show results
+		imshow(windowName, combinedMatches);	    // Display frame in window
+        waitKey(10);
+		count++;
+
 	}
+	waitKey(0);					                    // Press key to close window
 
-}
-
-/*
- *
- *  showThreshholdTrackbars - displays a window with 2 trackbars, respresenting the minimum and
- *  maximum hue to be factored into the threshholding
- *
- */
-void showThreshholdTrackbars(const std::string windowName)
-{
-
-    // Create a window and trackbars to populate it
-    trackHueMin = 0;
-    trackHueMax = 0;
-    createTrackbar("Hue Min.", windowName, &trackHueMin, MAX_COLOR_VALUE);
-    createTrackbar("Hue Max.", windowName, &trackHueMax, MAX_COLOR_VALUE);
+    // Return number of frames in video
+    return count;
 
 }
